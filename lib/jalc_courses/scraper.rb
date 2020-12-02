0c0563e41f5e39255fe1dd5c448e9e0c09f2adf9
mccray austin <tubach75@hotmail.com>
2020-12-02 16:14:45 +0000

    added precoded code to scraper

--- lib/jalc_courses/scraper.rb
+++ lib/jalc_courses/scraper.rb
@@ -3,7 +3,11 @@ require_relative "courses.rb"
 
 
 class JalcCourses::Scraper 
-  def self.this_test 
-    puts "Hello to you all"
-  end
+      def self.make_courses(site)
+      doc = Nokogiri::HTML(open(site))
+      doc.css('div.col.sqs-col-4.span-4').slice(2,10).each do |course|
+        Courses.new(course.css('div.sqs-block-content h4').text, course.css('div.sqs-block-content h3').text,
+          course.css('div.sqs-block-content p[1]').text, course.css('div.sqs-block-content p[2]').text)
+      end
+    end
 end
